a027d616635ad4fb68b155d3d4495be90be0402c
endorphin82 <endorphin82@gmail.com>
2018-09-27 20:24:09 +0300

    03.08.CSSinReact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
+// in css production
+import './contact.css';
+
 class Contact extends Component {
   // static propTypes = {
   //   name: PropTypes.string.isRequired,
