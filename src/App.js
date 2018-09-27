a97e727fd011e8a12bb28aa267c1ff5f51785e83
endorphin82 <endorphin82@gmail.com>
2018-09-27 19:53:40 +0300

    03.06.WorkingWithProps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+
+import Header from "./components/Header";
 import Contact from "./components/Contact";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
-        <Contact />
+        <Header branding="Contact Manager"/>
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555"/>
+        <Contact name="Karen Smith" email="karen@gmail.com" phone="333-333-3333"/>
       </div>
     );
   }
