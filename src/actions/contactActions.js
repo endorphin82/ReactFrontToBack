707315fbd4ea44b2dd2b53d2dadced5b6e53c00b
endorphin82 <endorphin82@gmail.com>
2018-10-06 03:15:15 +0300

    09.02.POST_DELETE_RequestDispatch

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -7,24 +7,32 @@ import {
 
 export const getContacts = () => async dispatch => {
   const res = await axios.get('https://jsonplaceholder.typicode.com/users');
-
   dispatch ({
     type: GET_CONTACTS,
     payload: res.data
   });
 };
 
-export const deleteContact = id => {
-  return {
-    type: DELETE_CONTACT,
-    payload: id
-  };
+export const deleteContact = id => async dispatch => {
+  try {
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+    dispatch ({
+      type: DELETE_CONTACT,
+      payload: id
+    });
+  } catch (e) {
+    dispatch ({
+      type: DELETE_CONTACT,
+      payload: id
+    });
+  }
 };
 
-export const addContact = contact => {
-  return {
+export const addContact = contact => async dispatch => {
+  const res = await axios.post('https://jsonplaceholder.typicode.com/users', contact);
+  dispatch ({
     type: ADD_CONTACT,
-    payload: contact
-  };
+    payload: res.data
+  });
 };
 
